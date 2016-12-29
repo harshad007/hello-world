d06133e0ded5f9ff7b18aa50be614396e503d13d
hvelapure <hvelapure@gmail.com>
2016-12-29 13:23:54 +0000

    added hello.java file

---
+++ hello.java
@@ -0,0 +1,10 @@
+#hellow world java program
+
+public class hello {
+
+    public static void main(String[] args) {
+        // Prints "Hello, World" to the terminal window.
+        System.out.println("Hello, World");
+    }
+
+}
